db90d3d6b4a6aec3dcdf4249822e1c5216c1a2c4
IAlwaysTap <yowssp881@gmail.com>
2026-04-02 17:49:59 -0400

    Create loader.lua

---
+++ fluxware/loader.lua
@@ -0,0 +1,32 @@
+local MarketplaceService = game:GetService("MarketplaceService")
+local Info = MarketplaceService:GetProductInfo(game.PlaceId)
+
+local GameInfo = {
+    Name = Info.Name,
+    Creator = {
+        Name = Info.Creator.Name
+    }
+}
+
+local Link = "https://raw.githubusercontent.com/IAlwaysTap/FluxWare/main/fluxware/"
+local Name = ""
+
+if GameInfo.Creator.Name == "Wiggity." then
+    Name = "bladeball.lua"
+elseif GameInfo.Name and GameInfo.Name:find("Prison Life") then
+    Name = "prisonlife.lua"
+elseif GameInfo.Creator.Name == "Uplift Games" then
+    Name = "adotpmepetspawn.lua"
+elseif GameInfo.Creator.Name == "ROLVe" then
+    Name = "arsenal.lua"
+elseif GameInfo.Creator.Name == "CRIMCORP" then
+    Name = "criminality.lua"
+elseif GameInfo.Creator.Name == "Wave of Brainrots" then
+    Name = "escapeabrainrot.lua"
+elseif GameInfo.Creator.Name == "Polygon Interactive" then
+    Name = "defusal.lua"
+end
+
+if Name ~= "" then
+    loadstring(game:HttpGet(Link .. Name))()
+end
